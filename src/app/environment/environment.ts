670de88bfadc1794f685220337c1ebd69a56e954
newjssports <sportsnewjs@gmail.com>
2025-01-01 12:58:12 +0500

    Env Setting

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false, // Indicates that this is the development environment
     //apiUrl: 'https://localhost:7276/api/' // Base URL for your .NET Core API in development
-    apiUrl: 'http://160.238.36.109:5000/api/' // Base URL for your .NET Core API in development
+    apiUrl: 'https://api.jsnewsports.shop/api/' // Base URL for your .NET Core API in development
   };
