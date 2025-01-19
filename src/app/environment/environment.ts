88233b00f7065830c77aac7fb5a971ecb5bd2650
newjssports <sportsnewjs@gmail.com>
2025-01-19 14:52:13 +0500

    API Base Url

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     //apiUrl: 'http://localhost:5001/api/' // Base URL for your .NET Core API in development
     //apiUrl: 'https://api.jsnewsports.shop/api/' // Base URL for your .NET Core API in development
     //apiUrl: 'http://127.0.0.1:6001/api/'
-    apiUrl: 'https://160.238.36.109:6001/api/', // Replace with your server's IP
+    apiUrl: 'http://160.238.36.109:6001/api/', // Replace with your server's IP
   };
