ba0c47a7bed53968ea2db6f25ff0bcca6f8ec104
newjssports <sportsnewjs@gmail.com>
2025-01-19 12:14:00 +0500

    change environment varibale Base URL

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false, // Indicates that this is the development environment
     //apiUrl: 'http://localhost:5001/api/' // Base URL for your .NET Core API in development
-    apiUrl: 'https://api.jsnewsports.shop/api/' // Base URL for your .NET Core API in development
+    //apiUrl: 'https://api.jsnewsports.shop/api/' // Base URL for your .NET Core API in development
+    apiUrl: 'http://127.0.0.1:6001/api/'
   };
